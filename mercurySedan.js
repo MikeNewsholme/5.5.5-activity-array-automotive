643f0e974ca92ad7cf4e87071190a975620e066c
mike.newsholme@gmail.com <mike.newsholme@gmail.com>
2022-05-16 10:26:35 -0400

    finished

---
+++ mercurySedan.js
@@ -0,0 +1,39 @@
+const vehicleModule = require('./vehicle').Vehicle
+
+class Car extends Vehicle{
+    constructor()
+    super() {
+ 
+        this.maximumPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maximumSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService =  false; 
+    }
+
+   loadPassenger(){
+       const num = passenger
+    if (this.passenger < this.maximumPassengers){
+    return num
+} else
+   {return maximumPassengers}
+   }
+   start(){
+       if (fuel > 0){
+           start == true
+       }
+   }
+   scheduleService(){
+       if (mileage > 30000){
+           maintenance == true
+       }
+   }
+}
+   
+
+
+
+
+
+    
